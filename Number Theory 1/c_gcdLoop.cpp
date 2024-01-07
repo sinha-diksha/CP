a2fa24f1ee450135da65a757429e684a04d0f9e5
diksha sinha <diksha@dikshas-MacBook-Air.local>
2024-01-07 17:32:52 +0530

    code added

---
+++ Number Theory 1/c_gcdLoop.cpp
@@ -0,0 +1,23 @@
+/*Problem statement
+Send feedback
+Calculate and return GCD of two given numbers x and y. Numbers are within range of Integer.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Input format :
+First line of Input will contain T(number of test cases), each test case follows as.
+x and y (separated by space)
+Output format :
+Print GCD of x and y for each test case in newline
+Constraints:
+1 <= T <= 10^5
+1 <= x, y <= 10^9
+Sample Input 1:
+1
+20 5
+Sample Output 1:
+5
+Sample Input 2:
+1
+96 14
+Sample Output 2:
+2*/
